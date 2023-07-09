22e5e0bc874dc4d4d413f175aeeb8954a805ed96
Aryann15 <rajlysm15@gmail.com>
2023-07-09 16:29:07 +0530

    importing all classes and making a numpy array for all images and  classNo

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import os
 
 path= 'myData'
 images= []
+classNo=[]
+
 myList = os.listdir(path)
 print(myList)
 
@@ -11,6 +13,11 @@ class_numbers = len(myList)
 for x in range(0,class_numbers):
     pic_list = os.listdir(path + '/' + str(x))
     for y in pic_list:
-        current_image = cv.imread(path+'/'+str(x)+ ' /' +y)
-        current_image = cv.resize(current_image(32,32))
+        current_image = cv.imread(path+'/'+str(x)+ '/' +y)
+        current_image = cv.resize(current_image,(32,32))
         images.append(current_image)
+        classNo.append(x)
+    # print(x)
+
+images = np.array(images)
+classNo = np.array(classNo)
