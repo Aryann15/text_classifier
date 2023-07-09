427fa0f3f19069e168859c8060509c8d8ed62b5e
Aryann15 <rajlysm15@gmail.com>
2023-07-09 16:19:06 +0530

    storing alll images in a 'image' list

--- train.py
+++ train.py
@@ -3,6 +3,14 @@ import cv2 as cv
 import os
 
 path= 'myData'
-
+images= []
 myList = os.listdir(path)
 print(myList)
+
+class_numbers = len(myList)
+for x in range(0,class_numbers):
+    pic_list = os.listdir(path + '/' + str(x))
+    for y in pic_list:
+        current_image = cv.imread(path+'/'+str(x)+ ' /' +y)
+        current_image = cv.resize(current_image(32,32))
+        images.append(current_image)
